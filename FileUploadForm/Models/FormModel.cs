4d519b80aedcdea2dae83b0e1f7dd6487ae67ef5
viyan <viyanomd@gmail.com>
2024-02-20 16:02:26 +0100

    fixed data annotations

--- FileUploadForm/Models/FormModel.cs
+++ FileUploadForm/Models/FormModel.cs
@@ -5,12 +5,14 @@ namespace FileUploadForm.Models
 {
     public class FormModel
     {
-        [Required]
+        [Required(ErrorMessage = "Email required!")]
         [EmailAddress(ErrorMessage = "Make sure email is correct!")]
         public string Email { get; set; }
-        [Required]
-        [FileExtensions(Extensions = ".doc,.docx,", ErrorMessage = "Please upload a valid file (.doc/.docx)!")]
-        public IBrowserFile File { get; set; } 
+        [Required(ErrorMessage = "File required!")]
+        public IBrowserFile File { get; set; }
+        [FileExtensions(Extensions = ".doc, .docx", ErrorMessage = "Please upload a valid file (.doc/.docx format)!")]
+
+        public string Name { get; set; }
 
     }
 }
