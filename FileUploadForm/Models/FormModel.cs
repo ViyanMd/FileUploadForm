97aa0baffadada30eb7432d815468d71b9ff8c6e
viyan <viyanomd@gmail.com>
2024-02-20 15:42:28 +0100

    added validaton

---
+++ FileUploadForm/Models/FormModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace FileUploadForm.Models
+{
+    public class FormModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Make sure email is correct!")]
+        public string Email { get; set; }
+        [Required]
+        [FileExtensions(Extensions = ".doc,.docx,", ErrorMessage = "Please upload a valid file (.doc/.docx)!")]
+        public IBrowserFile File { get; set; } 
+
+    }
+}
--- FileUploadForm/Services/BlobStorageService.cs
+++ FileUploadForm/Services/BlobStorageService.cs
@@ -1,5 +1,8 @@
 ﻿using Azure.Storage;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
 using FileUploadForm.Abstractions;
 using FileUploadForm.Models;
 using Microsoft.AspNetCore.Components.Forms;
@@ -8,86 +11,88 @@ namespace FileUploadForm.Services
 {
     public class BlobStorageService : IBlobStorageService
     {
-        private readonly string StorageAccount = "uploadformstorage";
-        private readonly string Key = "M+MlhoPyrtmCRfzB2LcMGWNBRQefCOxlztn29ofnOXvv3XUcXQeNsJbX/OVPLrNsghhExd7XPQt9+AStX5dSRg==";
+        private readonly ILogger<IBlobStorageService> Logger;
+        private readonly IConfiguration Configuration;
+        private readonly string StorageAccount = string.Empty;
+        private readonly string Key = string.Empty;
         private BlobContainerClient Container;
-        private ILogger<BlobStorageService> _logger;
 
-        public BlobStorageService(ILogger<BlobStorageService> _logger)
-        { 
+        public BlobStorageService(ILogger<IBlobStorageService> _logger, IConfiguration _configuration)
+        {
+            Logger = _logger;
+            Configuration = _configuration;
+
+            StorageAccount = Configuration.GetValue<string>("AzureConnnection:StorageAccount")!;
+            Key = Configuration.GetValue<string>("AzureConnnection:StorageKey")!;
+            
             var credentials = new StorageSharedKeyCredential(StorageAccount, Key);
             var blobUri = $"https://{StorageAccount}.blob.core.windows.net";
             var BlobServiceClient = new BlobServiceClient(new Uri(blobUri), credentials);
             Container = BlobServiceClient.GetBlobContainerClient("files");
-            this._logger = _logger;
         }
 
         public async Task<ResponseDTO> Upload(IBrowserFile blob)
         {
-/*            try
-            {*/
-                ResponseDTO response = new();
-
+            try
+            {
                 BlobClient client = Container.GetBlobClient(blob.Name);
+
+                Uri blobSASURI = await CreateServiceSASBlob(client);
+
+                BlobClient clientSAS = new BlobClient(blobSASURI);
 
                 using (Stream? data = blob.OpenReadStream())
                 {
-                    await client.UploadAsync(data);
+                    await clientSAS.UploadAsync(data);
                 }
 
-                response.Status = "File uploaded successfully";
-                response.Error = false;
-                response.Blob.Name = blob.Name;
-                response.Blob.URI = client.Uri.AbsoluteUri;
-
-                return response;
-/*            } catch (Exception ex)
+                return new ResponseDTO
+                {
+                    Status = "File uploaded successfully",
+                    Error = false,
+                    Blob = new BlobDTO
+                    {
+                        Name = blob.Name,
+                        URI = clientSAS.Uri.AbsoluteUri
+                    }
+                };
+            }
+            catch (Exception ex)
             {
-                _logger.LogError(ex, "Error uploading file");
-                return new ResponseDTO { Status = "Error uploading file", Error = true };
-            }   */
+                Logger.LogError(ex, ex.Message);
+                return new ResponseDTO { Status = "Filed to upload file!", Error = true };
+            }
 
         }
+        private async Task<Uri> CreateServiceSASBlob(BlobClient blobClient,string storedPolicyName = null)
+        {
+            if (blobClient.CanGenerateSasUri)
+            {
+                BlobSasBuilder sasBuilder = new BlobSasBuilder()
+                {
+                    BlobContainerName = blobClient.GetParentBlobContainerClient().Name,
+                    BlobName = blobClient.Name,
+                    Resource = "b"
+                };
 
-        //private readonly string StorageAccount = "uploadformstorage";
-        //private readonly string Key = "M+MlhoPyrtmCRfzB2LcMGWNBRQefCOxlztn29ofnOXvv3XUcXQeNsJbX/OVPLrNsghhExd7XPQt9+AStX5dSRg==";
-        //private BlobServiceClient Client;
+                if (storedPolicyName == null)
+                {
+                    sasBuilder.ExpiresOn = DateTimeOffset.UtcNow.AddHours(1);
+                    sasBuilder.SetPermissions(BlobSasPermissions.Read | BlobSasPermissions.Write);
+                }
+                else
+                {
+                    sasBuilder.Identifier = storedPolicyName;
+                }
 
-        //public StorageService()
-        //{
-        //    var credentials = new StorageSharedKeyCredential(StorageAccount, Key);
-        //    var blobUri = $"https://{StorageAccount}.blob.core.windows.net";
+                Uri sasURI = blobClient.GenerateSasUri(sasBuilder);
 
-        //    Client = new BlobServiceClient(new Uri(blobUri), credentials);
-
-        //}
-
-        //public async Task ListAllContainers()
-        //{
-        //    var containers = Client.GetBlobContainers();
-        //    foreach (var container in containers)
-        //    {
-        //        Console.WriteLine(container.Name);
-        //    }
-        //}
-
-        //public async Task<List<Uri>> Upload()
-        //{
-        //    var blobUri = new List<Uri>();
-
-        //    var filePath = "test.txt";
-
-        //    var container = Client.GetBlobContainerClient("files");
-
-        //    await container.CreateIfNotExistsAsync();
-
-        //    var blob = container.GetBlobClient(filePath);
-
-        //    await blob.UploadAsync(filePath, true);
-
-        //    blobUri.Add(blob.Uri);
-
-        //    return blobUri;
-        //}
+                return sasURI;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
